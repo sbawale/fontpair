2277736839f1b6bcc00a190df6784d42336b7c88
Sara <44151312+sbawale@users.noreply.github.com>
2019-11-10 16:02:25 -0800

    uploaded latest files
    apparently the last several (many) commits I made didn't actually go through... so here is a large file dump

--- helper_functions.py
+++ helper_functions.py
@@ -1,7 +1,19 @@
-import json, urllib.request, re, csv
+import csv
 import pandas as pd
 import numpy as np
-from contextlib import closing
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def split_train_test_val(data, train_ratio):
+    data_copy = data.copy()
+    train_set = data_copy.sample(frac=train_ratio, random_state=0)
+    test_set = data_copy.drop(train_set.index)
+
+    train_copy = train_set.copy()
+    train_set = train_copy.sample(frac=train_ratio,random_state=0)
+    val_set = train_copy.drop(train_set.index)
+
+    return train_set, test_set, val_set
+
 
 def check_if_serif(family,category):
     if category == 'serif' or 'serif' in family:
@@ -34,46 +46,9 @@ def get_unlabeled_families(filename):
 def get_unique_strings(dataset,feature_index):
     unique = []
     for font in dataset.itertuples(index=False,name=None):
-        # Split current value into individual words
+        # Split current value into individual words based on whitespace
         feature = font[feature_index].split()
         for word in feature:
             if word not in unique:
                 unique.append(word)
     return unique
-
-def vectorize_font(font,names,families,features):
-    # colNames =  ['name','family','category','is_body','is_serif','is_italic','weight']
-    # features = ['body','display','handwriting','monospace','serif','sans-serif','italic','weight']
-
-    # Instantiate sub vectors
-    name_vector = [0]*len(names)
-    family_vector = [0]*len(families)
-    feature_vector = [0]*len(features)
-
-    # Convert name and family to list of individual words
-    name = font[0].split()
-    family = font[1].split()
-
-    # Convert name and family to vector
-    for i in range(0,len(names)):
-        if names[i] in name:
-            name_vector[i] = 1
-
-    for i in range(0,len(families)):
-        if families[i] in family:
-            family_vector[i] = 1
-
-    # Get the remaining features, check values for each feature
-    feature_vector[0] = font[3] # body
-    feature_vector[1] = int(font[2] == 'display') # category
-    feature_vector[2] = int(font[2] == 'handwriting') # category
-    feature_vector[3] = int(font[2] == 'monospace') # category
-    feature_vector[4] = font[4] # serif
-    feature_vector[5] = int(not font[4]) # sans-serif
-    feature_vector[6] = font[5] # italic
-    feature_vector[7] = font[6]/1000 # weight
-    # need to scale weight somehow... maybe divide by 1000? or do another bag of words?
-
-    # Combine sub vectors into complete font vector
-    font_vector = name_vector + family_vector + feature_vector
-    return font_vector
--- preprocess_data_gf.py
+++ preprocess_data_gf.py
@@ -1,23 +1,24 @@
 import json, urllib.request, re
 import pandas as pd
+import numpy as np
 from contextlib import closing
 from helper_functions import *
 
-def preprocess_data_gf():
+def filter_data_gf():
     # Initialize font list, column/feature names, array of font weights, and get ambiguous families
     gf = []
-    label_me = []
-    col_names =  ['name','family','category','is_body','is_serif','is_italic','weight']
-    fontWeights = ['Thin','Extra Light','Light','Regular','Medium',
-                   'Semi Bold','Bold','Extra Bold','Black']
     [families,serifs] = get_unlabeled_families('label_by_hand.csv')
-    # Corresponding numerical weights: [100, 200, 300, 400, 500, 600, 700, 800, 900]
+    col_names =  ['name','family','category','is_body','is_serif','is_italic','weight']
+    # font_weights = ['Thin','Extra Light','Light','Regular','Medium',
+    #                'Semi Bold','Bold','Extra Bold','Black']
+    font_weights = ['thin','extralight','light','regular','medium','semibold','bold','extrabold','black']
+    font_weights_num = [100, 200, 300, 400, 500, 600, 700, 800, 900]
 
-    # *************** IMPORT AND CLEAN GOOGLE FONTS DATA ***************
+    # *************** IMPORT AND RESTRUCTURE GOOGLE FONTS DATA ***************
 
-    with open('google-fonts.json') as json_file:
-        gfData = json.load(json_file)
-        fontlist = gfData['items'] # Get actual list of fonts (Google wraps them in a 2-column list for some reason)
+    with open('data_google_fonts.json') as json_file:
+        data = json.load(json_file)
+        fontlist = data['items'] # Get actual list of fonts (Google wraps them in a 2-column list for some reason)
         print("Loading Google Fonts data...\n")
         for font in fontlist:
             # Check if font uses Latin alphabet, otherwise skip
@@ -33,7 +34,8 @@ def preprocess_data_gf():
             is_body = int(category != 'display')
             is_serif = check_if_serif(family.lower(),category)
             is_italic = 0 # default is 0: not is_italic
-            weight = 400 # default is 400: regular
+            # is_italic = 'roman'
+            weight = 'regular' # default is 400: regular
 
             # Get serif status for fonts with is_serif = -1
             if is_serif == -1:
@@ -41,24 +43,37 @@ def preprocess_data_gf():
                     if f == family:
                         is_serif = serifs[i]
 
+            # if is_serif:
+            #     is_serif = 'serif'
+            # else:
+            #     is_serif = 'sans-serif'
+
+            # if is_body:
+            #     is_body = 'body'
+            # else:
+            #     is_body = 'heading'
+
             # Check for font variants
             variants = font['variants']
             if len(variants) > 1:
                 for var in variants:
                     # Get weight
-                    varWeight = var.split("00")
-                    if len(varWeight) == 1:
+                    var_weight = var.split("00")
+                    if len(var_weight) == 1:
                         weight = 400
                     else:
-                        weight = int(varWeight[0])*100
+                        weight = int(var_weight[0])*100
 
                     # Get name based on weight
-                    weightIndex = int((weight/100)-1)
-                    name = family + " " + fontWeights[weightIndex]
+                    weight_idx = int((weight/100)-1)
+                    # name = family + " " + font_weights[weight_idx]
+                    name = family + " " + str(weight)
+                    weight = font_weights[weight_idx]
 
                     # Check if is_italic
-                    if 'italic' in varWeight:
+                    if 'italic' in var_weight:
                         is_italic = 1
+                        # is_italic = 'italic'
                         name = name + " Italic"
 
                     # Create tuple to be appended to list
@@ -72,7 +87,7 @@ def preprocess_data_gf():
                     current['weight'] = weight
 
                     # Print to console and append to gf list
-                    print(name)
+                    # print(name)
                     gf.append(current)
             else:
                 name = family
@@ -88,20 +103,88 @@ def preprocess_data_gf():
                 current['weight'] = weight
 
                 # Print to console and append to gf list
-                print(name)
+                # print(name)
                 gf.append(current)
 
+    # *************** STANDARDIZE AND RETURN DATA ***************
+
     # Convert gf list to dataframe
-    dfGF = pd.DataFrame(gf, columns = col_names)
-    print("\nGoogle Fonts data successfully loaded!\n")
+    dfGF = pd.DataFrame(gf,columns=col_names)
+    dfGF.to_csv(r'cleanedGF.csv', index = None, header=True)
+    # print(dfGF.head(10))
+    print("\nGoogle Fonts data successfully processed!\n")
     return dfGF
 
-# ***************** TESTING *****************
+# def get_bag_of_words(font,include_family):
+#     # Convert all features (weight, category, etc.) to single bag of words
+#     bags_of_words = []
 
-# test = preprocess_data_gf()
-# with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-#     print(test)
-# test.to_csv(r"testgf.csv", index = None, header=True)
+#     if include_family:
+#         family = font[1].split()
+#         no_digits = "".join(filter(lambda x: not x.isdigit(), family))
+#         curr_bag = str(no_digits) + ' ' + font[2]
+#     else:
+#         curr_bag = font[2]
 
-# test2 = get_unlabeled_families(test)
-# print(test2)
+#     # Get body value
+#     if font[3] == 1: body = ' body'
+#     else: body = ' heading'
+
+#     # Get serif value
+#     if font[4] == 1: serif = ' serif'
+#     else: serif = ' sans-serif'
+
+#     # Get italic value
+#     if font[5] == 1: italic = ' italic'
+#     else: italic = ' roman'
+
+#     # Add the current bag to the list of all bags
+#     curr_bag += body + serif + italic + ' ' + font[6]
+#     bags_of_words.append(curr_bag)
+
+def get_font_vectors(fonts, include_family):
+    # Convert all features (weight, category, etc.) to single bag of words
+    bags_of_words = []
+
+    for font in fonts.itertuples(index=False,name=None):
+        if include_family:
+            family = font[1].split()
+            no_digits = "".join(filter(lambda x: not x.isdigit(), family))
+            curr_bag = str(no_digits) + ' ' + font[2]
+        else:
+            curr_bag = font[2]
+
+        # Get body value
+        if font[3] == 1: body = ' body'
+        else: body = ' heading'
+
+        # Get serif value
+        if font[4] == 1: serif = ' serif'
+        else: serif = ' sans-serif'
+
+        # Get italic value
+        if font[5] == 1: italic = ' italic'
+        else: italic = ' roman'
+
+        # Add the current bag to the list of all bags
+        curr_bag += body + serif + italic + ' ' + font[6]
+        bags_of_words.append(curr_bag)
+
+    # Convert bags of words to TF-IDF vectors
+    print("\nattempting to create vectors...\n")
+    tfidf = TfidfVectorizer(ngram_range=(1, 1), min_df=0.0001)
+    tfidf_matrix = tfidf.fit_transform(bags_of_words)
+    vectors = tfidf_matrix
+    print("successfully vectorized fonts!")
+
+    # Create new dataframe with 3 columns: font name, original bag of words, vector
+    print("\ncreating new dataframe...\n")
+    font_dict = fonts[['name']]
+    font_dict.insert(1, 'bag_of_words', bags_of_words)
+    font_dict.insert(2, 'tfidf_vector', vectors)
+    font_dict.insert(3, 'id', font_dict.index.tolist())
+    font_dict.set_index('name', drop=True, append=False, inplace=True, verify_integrity=False)
+    # font_dict.to_csv('vectortest.csv')
+    print("\ndataframe created!\n")
+
+    return vectors, font_dict
