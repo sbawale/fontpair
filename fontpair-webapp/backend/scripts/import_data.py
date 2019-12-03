a6e03aae0331971cfeadf1aed982af538527d912
Sara <sara.bawale@gmail.com>
2019-12-03 15:20:18 -0800

    fixed import data script

--- fontpair-webapp/backend/scripts/import_data.py
+++ fontpair-webapp/backend/scripts/import_data.py
@@ -8,6 +8,18 @@ from categories.models import *
 def run():
     # Import fonts
     with open('data/cleanedGF.csv') as csvfile_fonts:
+        WEIGHTS = {
+            'thin': 'Thin',
+            'extralight': 'Extra Light',
+            'light': 'Light',
+            'regular': 'Regular',
+            'medium': 'Medium',
+            'semibold': 'Semi Bold',
+            'bold': 'Bold',
+            'extrabold': 'Extra Bold',
+            'black': 'Black',
+        }
+
         reader = csv.DictReader(csvfile_fonts)
         for row in reader:
             f = Font(name=row['name'],
@@ -16,7 +28,8 @@ def run():
                  is_body=row['is_body'],
                  is_serif=row['is_serif'],
                  is_italic=row['is_italic'],
-                 weight=row['weight'],
+                 weight = WEIGHTS.get(row['weight']),
+                 # weight=row['weight'],
                  url=row['url'])
             f.save()
 
